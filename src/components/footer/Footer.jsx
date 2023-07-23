82c13ef5b0652aa34e7d11973a1f48f6560f742b
adeleyepaul <adeleyepaul>
2023-07-23 13:22:11 +0100

    fixed error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,18 +11,18 @@ const Footer = () => {
       <a href="#" className='footer__logo'>ENVELOPE</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="iridescent-malasada-659400">Home</a></li>
+        <li><a href="iridescent-malasada-659400/about">About</a></li>
+        <li><a href="iridescent-malasada-659400/experience">Experience</a></li>
+        <li><a href="iridescent-malasada-659400/services">Services</a></li>
+        <li><a href="iridescent-malasada-659400/testimonials">Testimonials</a></li>
+        <li><a href="iridescent-malasada-659400/contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
-        <a href="#"><FiFacebook/></a>
-        <a href="#"><BsInstagram/></a>
-        <a href="#"><BsTwitter/></a>
+        <a href="facebook.com/adeleyepaul"><FiFacebook/></a>
+        <a href="instagram.com/nobulplus"><BsInstagram/></a>
+        <a href="twitter.com/nobulplus"><BsTwitter/></a>
       </div>
 
       <div className="footer__copyright">
