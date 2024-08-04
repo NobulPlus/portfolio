6b4123058aca6bd8dee383dac3979ae337e0cc99
NobulPlus <chenko4me@gmail.com>
2024-08-04 21:59:53 +0100

    linkedin link fixed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/adeleye-paul-023206123/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/adeleyepaul/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
         <a href="https://github.com/adeleyepaul" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
         {/* <a href="http://dribbble.com" target="_blank"><FiDribbble/></a> */}
     </div>
